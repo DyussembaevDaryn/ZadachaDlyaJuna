285e2626daaa4f13c60159a5be1609807476a606
Daryn Dyussembaev <Darynchik1@mail.ru>
2024-04-17 15:23:24 +0600

    Stop tracking folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import {User} from "./services/users/models/user.models";
+import {UsersService} from "./services/users/users.service";
+import {UsersApiService} from "./services/users-api.service";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl:'app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'my-first-project';
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersListComponent } from './components/users-list/users-list.component';
+import {Routes,RouterModule} from "@angular/router";
+import { HttpClientModule } from '@angular/common/http';
+import { UserCardComponent } from './components/user-card/user-card.component';
 
+const appRoutes: Routes =[
+  {path:'users', component: UsersListComponent}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersListComponent,
+    UserCardComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
